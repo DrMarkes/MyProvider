25d38d9d967210e65196f42839e42269f4591db7
DrMarkes <markovandrew@yandex.ru>
2016-01-26 21:52:58 +0700

    start

--- app/src/main/java/ru/drmarkes/myprovider/MyContactsProvider.java
+++ app/src/main/java/ru/drmarkes/myprovider/MyContactsProvider.java
@@ -1,7 +1,41 @@
 package ru.drmarkes.myprovider;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
 /**
  * Created by Андрей on 26.01.2016.
  */
-public class MyContactsProvider {
+public class MyContactsProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
 }
